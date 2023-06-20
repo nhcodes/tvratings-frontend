2224e9bf1b26728ff2855556fcfc7caa50be4e15
nhcodes <nhdotcodes@gmail.com>
2023-06-20 17:16:08 +0200

    show message if user follows no shows

--- js/profile_module.js
+++ js/profile_module.js
@@ -2,16 +2,24 @@
 
 function getFollowListHtml(shows) {
     return `
-        <div class="list-group list-group-flush">
-            ${loop(shows, (show) => `
-                <a class="list-group-item list-group-item-action d-flex flex-row align-items-center" href="?showId=${show["showId"]}" target="_blank">
-                    <span class="flex-fill">${show["title"]}</span>
-                    <button class="btn btn-link link-secondary" onclick="follow('${show["showId"]}', false);return false">
-                        <span class="bi bi-x"></span>
-                    </button>
-                </a>
-            `)}
-        </div>
+        ${conditional(shows.length > 0, `
+
+            <div class="list-group list-group-flush">
+                ${loop(shows, (show) => `
+                    <a class="list-group-item list-group-item-action d-flex flex-row align-items-center" href="?showId=${show["showId"]}" target="_blank">
+                        <span class="flex-fill">${show["title"]}</span>
+                        <button class="btn btn-link link-secondary" onclick="follow('${show["showId"]}', false);return false">
+                            <span class="bi bi-x"></span>
+                        </button>
+                    </a>
+                `)}
+            </div>
+            
+        `, `
+
+            <p class="mx-auto my-5">no followed shows</p>
+            
+        `)}
     `;
 }
 
